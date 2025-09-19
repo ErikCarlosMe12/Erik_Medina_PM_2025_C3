4145814ad63e5166ad69af0370a8aa84920a1734
Erik <medipaerik@gmail.com>
2025-09-18 21:42:12 -0400

    Agregue Tarea 1 PM_C3

---
+++ Tarea 1/Programa 1.13/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* Aplicacion de operadores. */
+void main(void)
+{
+int i = 5, j = 4, k, l, m;
+k = !i * 3 + --j * 2 - 3;
+printf("\nEl valor de k es: %d", k);
+
+l = ! (!i || 1 && 0) && 1;
+printf("\nEL valor de 1 es: %d", 1);
+
+m = (k = (! (12 > 10)), j = (10 || 0) && k, (! (k || j)));
+printf("\nEL valor de m es: %d",m);
+}
+
---
+++ Tarea 1/Programa 1.6/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/* Aplicacion de operadores. */
+
+void main (void)
+{
+int i = 15, j, k, l;
+
+j = (15 > i--) > (14 < ++i);
+printf("\nEl valor de j es: &d", j);
+
+k = ! (‘b’ != ‘b’) > (!i - 1);
+printf("\nEL valor de k es: %d", k);
+
+l = (!(34 > (70 % 2))|| 0);
+printf("\nEl valor de 1 es: %d", l);
+}
